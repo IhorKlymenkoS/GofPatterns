4b726c032e9bbbadb44ef72494c64be8e7492f93
IhorKlymenko <ihorklymenkoc@gmail.com>
2022-06-14 10:13:38 +0300

    Upd main

--- GofPatterns/Program.cs
+++ GofPatterns/Program.cs
@@ -6,6 +6,36 @@ namespace GofPatterns
     {
         static void Main(string[] args)
         {
+            //Proxy
+            //Bank bank = new OneHundredPercentReliability();
+            //int money = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(bank.TransferMoney(money));
+
+            //ChainOfResponsibility
+            //Shop shop = new Shop();
+            //Console.WriteLine("What do you want to buy? sword, axe, amulet?");
+            //string product = Console.ReadLine();
+            //Console.WriteLine("It won't be cheap! Don't spare money. (enter amount of money)");
+            //int money = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(shop.GetTheJobDone(money, product));
+
+            //Facade
+            //TheWeather weather = new TheWeather();
+            //Console.WriteLine(weather.CheckTheWeather());
+
+            //Observer
+            //Observer observer = new Observer();
+            //Console.WriteLine(observer.WinLottery());
+
+            //Clone
+            //Prototype prototype = new ConcretePrototype1(1, "white", "Dolly sheep");
+            //Prototype clone = prototype.Clone();
+            //Console.WriteLine(clone.Id + " " + clone.Name + " " + clone.Color);
+            //prototype = new ConcretePrototype2(2, "black", "Molly sheep");
+            //clone = prototype.Clone();
+            //Console.WriteLine(clone.Id + " " + clone.Name + " " + clone.Color);
         }
     }
+
 }
+
